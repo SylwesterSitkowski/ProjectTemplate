d933030f16496d6753647a90eaa6e161e94c3abf
ssitkowx <sitkowskis@interia.pl>
2022-10-19 13:36:28 +0200

    Update

--- Readme.md
+++ Readme.md
@@ -1,29 +1,35 @@
-Template for C embedded projects.
+#Template for C embedded projects.
 
-I. Description:
+##I. Description:
 The assumption is to create a project that separates firmware (hardware libraries) from software (target functionalities).
 
-II. Structure:
+##II. Structure:
 The embedded project has been divided into three parts:
 
 External:
 This is where documents and external libraries are stored.
 
 Project:
-   Logic:
-   This is where environment-specific but hardware-independent code is stored.
+   - Logic:
+     This is where environment-specific but hardware-independent code is stored.
    
-   Hardware:
-   This is where the environment-specific code is stored.
+   - Hardware:
+     This is where the environment-specific code is stored.
    
 Tests:
-   BlackBox:
-   This is where the black-box tests are stored.
+   - BlackBox:
+     This is where the black-box tests are stored.
    
-   WhiteBox:
-   This is where the white-box tests are stored.
+   - WhiteBox:
+     This is where the white-box tests are stored.
+     
+     - Hardware
+     - Logic
+       - Fakes,
+       - Fixtures.
+       
 
-III. Interface:
-Project is based on the interfaces. Every unit is represented by one object.
-Object has end of source file scope and can be extended with extern keyword.
-
+##III. Interface:
+- Project is based on the structures interfaces,
+- Every unit (.h and .c file) is represented by one object,
+- Object has end of source file scope which be extended with extern keyword.
--- Tests/WhiteBox/Logic/Fixtures/App/AppFixture.cxx
+++ Tests/WhiteBox/Logic/Fixtures/App/AppFixture.cxx
@@ -5,7 +5,6 @@
 extern "C"
 {
     #include "App.h"
-    #include "Utils.h"
 }
 
 #include "AppFixture.hxx"
@@ -24,58 +23,7 @@ SHARE_APP
 
 TEST_F (AppFixture, CheckEntryIntoManualMode)
 {
-    SetManual    (true);
-    gApp.Process (&gApp);
-
-    EXPECT_EQ    (gApp.GetState (), eManual);
-    EXPECT_TRUE  (gSettings.Communication.Tcms.Output.IsManuallyReleased);
-}
-
-TEST_F (AppFixture, CheckEntryIntoLockOutMode)
-{
-    gSettings.Communication.Tcms.Output.IsLockOutState                   = false;
-    gSettings.Communication.Tcms.Output.IsRetractedDefault               = true;
-    gSettings.Communication.Tcms.Output.IsDeploymentDefault              = true;
-    gSettings.Communication.Tcms.Output.IsAccessNotGuaranteed            = false;
-    gSettings.Communication.Tcms.Output.IsFailedToDeployBecauseObstacle  = true;
-    gSettings.Communication.Tcms.Output.IsFailedToRetractBecauseObstacle = true;
-
-    SetLockOut   (true);
-    gApp.Process (&gApp);
-
-    EXPECT_EQ    (gApp.GetState (), eLockOut);
-    EXPECT_TRUE  (gSettings.Communication.Tcms.Output.IsLockOutState);
-    EXPECT_FALSE (gSettings.Communication.Tcms.Output.IsRetractedDefault);
-    EXPECT_FALSE (gSettings.Communication.Tcms.Output.IsDeploymentDefault);
-    EXPECT_TRUE  (gSettings.Communication.Tcms.Output.IsAccessNotGuaranteed);
-    EXPECT_FALSE (gSettings.Communication.Tcms.Output.IsFailedToDeployBecauseObstacle);
-    EXPECT_FALSE (gSettings.Communication.Tcms.Output.IsFailedToRetractBecauseObstacle);
-}
-
-TEST_F (AppFixture, CheckEntryIntoNominalMode)
-{
-    SetNominal (true);
-    EXPECT_EQ  (gApp.GetState (), eNominal);
-}
-
-TEST_F (AppFixture, CheckEntryIntoUnknownMode)
-{
-    gSettings.Io.Input.IsLockOut.One        = true;
-    gSettings.Io.Input.IsClosedPosition.One = false;
-
-    EXPECT_EQ (gApp.GetState (), eUnknown);
-}
-
-TEST_F (AppFixture, CheckBackToEnableModeAtNonZeroSpeed)
-{
-    gApp.eNominal                          = eDetectObstaclesDuringOpening;
-    gApp.DetectObstaclesDuring.eOpening    = eDeploy;
-    gSettings.Io.Input.IsZeroVelocityTL    = false;
-    gSettings.App.Motor.IsObstacleDetected = false;
-
-    SetNominal   (true);
-    gApp.Process (&gApp);
-    EXPECT_EQ    (gApp.eNominal, eEnable);
+    EXPECT_TRUE  (true);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
